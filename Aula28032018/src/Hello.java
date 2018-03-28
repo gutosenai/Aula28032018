dcd6f8556f4b4edade872e3dbc0238f8c2e53be3
Luiz Augusto <37881647+gutosenai@users.noreply.github.com>
2018-03-28 17:03:24 -0300

    Update Hello.java

--- Aula28032018/src/Hello.java
+++ Aula28032018/src/Hello.java
@@ -2,8 +2,9 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
-
+		
 		System.out.println("SENAI melhor curso");
+		System.out.println("BATATA");
 
 	}
 }
